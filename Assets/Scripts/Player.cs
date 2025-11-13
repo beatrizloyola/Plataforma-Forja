9855bac9be9ec832061f2f445f58e57d6987a696
Beatriz Loyola <beatrizvasconcelos0802@gmail.com>
2025-11-13 15:52:16 -0300

    Movimento Vertical

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,6 +3,12 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float velocidade = 5f;
+    public float forcaPulo = 10f;
+    public Transform chaoTeste;
+    public float chaoTesteRaio = 0.2f;
+    public LayerMask chaoCamada;
+    public bool noChao;
+
     private Rigidbody2D rb;
 
     void Start()
@@ -14,5 +20,14 @@ public class Player : MonoBehaviour
     {
         float entrada = Input.GetAxis("Horizontal");
         rb.linearVelocity = new Vector2(entrada * velocidade, rb.linearVelocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Space) && noChao)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, forcaPulo);
+        }
+    }
+
+    private void FixedUpdate() {
+        noChao = Physics2D.OverlapCircle(chaoTeste.position, chaoTesteRaio, chaoCamada);
     }
 }
