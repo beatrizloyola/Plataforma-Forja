ae96c7214a0dc2d4486056a55418935bde80e7c6
Beatriz Loyola <beatrizvasconcelos0802@gmail.com>
2025-11-18 20:19:58 -0300

    TextMesh Pro + Contador de Coletáveis Fase 2

--- Assets/Scripts/Collectible.cs
+++ Assets/Scripts/Collectible.cs
@@ -14,6 +14,7 @@ public class Collectible : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.CompareTag("Player"))
         {
+            CollectiblesManager.Instance.CollectItem();
             Destroy(gameObject);
         }
     }
---
+++ Assets/Scripts/CollectiblesManager.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class CollectiblesManager : MonoBehaviour
+{
+    public static CollectiblesManager Instance;
+    
+    public TextMeshProUGUI counterText;
+    
+    private int totalCollectibles;
+    private int remainingCollectibles;
+    
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    
+    private void Start()
+    {
+        totalCollectibles = GameObject.FindGameObjectsWithTag("Collectible").Length;
+        remainingCollectibles = totalCollectibles;
+        UpdateUI();
+    }
+    
+    public void CollectItem()
+    {
+        remainingCollectibles--;
+        UpdateUI();
+        
+        if (remainingCollectibles <= 0)
+        {
+            Debug.Log("Todos os coletáveis foram coletados!");
+            // Aqui você pode adicionar lógica de vitória
+        }
+    }
+    
+    private void UpdateUI()
+    {
+        counterText.text = "Items: " + remainingCollectibles;
+    }
+}
