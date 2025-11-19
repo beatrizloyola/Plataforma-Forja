4a70169b9450ad1892ebe4b2f3014ea8e834bed1
Beatriz Loyola <beatrizvasconcelos0802@gmail.com>
2025-11-18 21:38:40 -0300

    Fundo + Ajustes nas Fases

--- Assets/Scripts/DoorController.cs
+++ Assets/Scripts/DoorController.cs
@@ -4,14 +4,22 @@ public class DoorController : MonoBehaviour
 {
     public Color lockedColor = Color.red;
     public Color unlockedColor = Color.green;
-    
+    public bool startUnlocked = false;
     private bool isUnlocked = false;
     private SpriteRenderer spriteRenderer;
     
     private void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
-        spriteRenderer.color = lockedColor;
+        if (startUnlocked || SceneManager.GetActiveScene().buildIndex == 0)
+        {
+            isUnlocked = true;
+            spriteRenderer.color = unlockedColor;
+        }
+        else
+        {
+            spriteRenderer.color = lockedColor;
+        }
     }
     
     public void UnlockDoor()
